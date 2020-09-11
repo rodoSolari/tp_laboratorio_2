ff1cc70beb15d2bef661b7ed6977917207791027
rodoSolari <63050260+rodoSolari@users.noreply.github.com>
2020-09-11 17:45:40 -0300

    Vuelvo a subir TP1 por error de nombre de proyecto
    Se tenia que modificar el nombre del proyecto TP1 a "MiCalculadora"

---
+++ TP1/Entidades/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        string ValidarOperador(char operador)
+        {
+            return "hola";
+        }
+    }
+
+    public class Numero
+    {
+
+        private double numero;
+
+        public string SetNumero
+        {
+            set { this.numero = ValidarNumero(value); }
+        }
+
+        public Numero()
+        {
+            numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+
+        }
+
+        public static string BinarioDecimal(string binario)
+        {
+            string resultado = "";
+            return resultado;
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string resultado = "";
+            return resultado;
+        }
+
+        public static string DecimalBinario(string numero)
+        {
+            string resultado = "";
+            return resultado;
+        }
+
+        public static bool esBinario(string binario)
+        {
+            return true;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double resultado = 0;
+            return resultado;
+        }
+
+        public static double ValidarNumero(string numero)
+        {
+            return 0;
+
+        }
+
+    }
+}
---
+++ TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpiar()
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Text = "";
+        }
+    }
+}
