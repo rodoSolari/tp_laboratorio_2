37d73d7570aee2fbce696d494c600af862b96494
rodoSolari <63050260+rodoSolari@users.noreply.github.com>
2020-09-10 22:53:20 -0300

    Add files via upload

---
+++ TP1/TP1/FormCalculadora.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TP1
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCalculadora));
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            resources.ApplyResources(this.btnOperar, "btnOperar");
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnCerrar
+            // 
+            resources.ApplyResources(this.btnCerrar, "btnCerrar");
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnLimpiar
+            // 
+            resources.ApplyResources(this.btnLimpiar, "btnLimpiar");
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnConvertirADecimal
+            // 
+            resources.ApplyResources(this.btnConvertirADecimal, "btnConvertirADecimal");
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
+            // 
+            // btnConvertirABinario
+            // 
+            resources.ApplyResources(this.btnConvertirABinario, "btnConvertirABinario");
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
+            // 
+            // lblResultado
+            // 
+            resources.ApplyResources(this.lblResultado, "lblResultado");
+            this.lblResultado.Name = "lblResultado";
+            // 
+            // txtNumero1
+            // 
+            resources.ApplyResources(this.txtNumero1, "txtNumero1");
+            this.txtNumero1.Name = "txtNumero1";
+            // 
+            // txtNumero2
+            // 
+            resources.ApplyResources(this.txtNumero2, "txtNumero2");
+            this.txtNumero2.Name = "txtNumero2";
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Items.AddRange(new object[] {
+            resources.GetString("cmbOperador.Items"),
+            resources.GetString("cmbOperador.Items1")});
+            resources.ApplyResources(this.cmbOperador, "cmbOperador");
+            this.cmbOperador.Name = "cmbOperador";
+            // 
+            // FormCalculadora
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmbOperador);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnOperar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalculadora";
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.ComboBox cmbOperador;
+    }
+}
+
---
+++ TP1/TP1/FormCalculadora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1
+{
+    partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpiar() {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Text = "";
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
