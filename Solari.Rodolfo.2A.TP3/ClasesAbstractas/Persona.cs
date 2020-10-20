877f69f8fe841fe951d8bff01ef8624c3c2f96f3
rodoSolari <rodosolari033@gmail.com>
2020-10-20 17:00:16 -0300

    TP3 con proyectos armados, clases persona y universitario(Incompletos)

---
+++ Solari.Rodolfo.2A.TP3/ClasesAbstractas/Persona.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    abstract public class Persona
+    {
+        #region Atributos
+        string nombre;
+        string apellido;
+        ENacionalidad nacionalidad;
+        int dni;
+        #endregion
+
+        #region Enumerados
+        public enum ENacionalidad {
+            Argentino,Extranjero
+        }
+        #endregion
+
+        #region Propiedades
+        abstract public string Apellido { get; set; }
+
+        abstract public int DNI { get; set; }
+
+        abstract public ENacionalidad Nacionalidad { get; set; }
+
+        abstract public string Nombre { get; set; }
+
+        abstract public string StringToDNI { set; }
+        #endregion
+
+        #region Metodos
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) : this(nombre,apellido,nacionalidad)
+        { 
+        
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        { 
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public int ValidarDni(ENacionalidad nacionanidad, int dato)
+        {
+            return 0;
+        }
+
+        public int ValidarDni(ENacionalidad nacionalidad, string dato) 
+        {
+            return 0;
+        }
+
+        public string validarNombreApellido(string dato) 
+        {
+            return "";
+        }
+        #endregion
+
+
+    }
+}
---
+++ Solari.Rodolfo.2A.TP3/ClasesAbstractas/Universitario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    abstract class Universitario : Persona
+    {
+        #region Atributos
+        private int legajo;
+        #endregion
+
+        #region Constructores
+        public Universitario() 
+        {
+        
+        }
+
+        public Universitario(int legajo,string nombre, string apellido, string dni, ENacionalidad nacionalidad) : base(nombre,apellido,dni,nacionalidad)
+        {
+            this.legajo = legajo;
+        }
+
+        #endregion
+
+        #region Metodos
+        protected virtual string MostrarDatos() 
+        {
+            return "";
+        }
+
+        protected abstract string ParticiparEnClase();
+
+        #endregion
+
+        #region Sobrecarga de operadores
+
+        public static bool operator ==(Universitario pg1, Universitario pg2) 
+        {
+            return (pg1.Nacionalidad == pg2.Nacionalidad && (pg1.DNI == pg2.DNI || pg1.legajo == pg2.legajo) );
+        }
+
+        public static bool operator !=(Universitario pg1, Universitario pg2)
+        {
+            return !(pg1 == pg2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool rta = false;
+            if (obj is Universitario)
+            {
+                rta = this == (Universitario)obj;
+            }
+            return rta;
+        }
+
+        #endregion
+
+    }
+}
