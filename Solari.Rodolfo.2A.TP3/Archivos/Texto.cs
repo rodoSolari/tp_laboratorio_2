48bd9a59c45c5fe768b648f9a70b7929c121e582
rodoSolari <rodosolari033@gmail.com>
2020-10-30 02:16:28 -0300

    modificado clases de Archivo, elimino using ClasesAbstractas sin usar en clasesInstanciables y quitando comentacion del main(vuelvo a pushear por equivocarme al no copiar los archivos que iba a subir)

--- Solari.Rodolfo.2A.TP3/Archivos/Texto.cs
+++ Solari.Rodolfo.2A.TP3/Archivos/Texto.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excepciones;
 
 namespace Archivos
 {
@@ -10,12 +12,34 @@ namespace Archivos
     {
         public bool Guardar(string archivo, string datos)
         {
-            return true;
+            try
+            {
+                using (StreamWriter archivoEscritura = new StreamWriter(archivo,true))
+                {
+                    archivoEscritura.WriteLine(datos);
+                    return true;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new ArchivosException(e);
+            }
         }
 
         public bool Leer(string archivo, out string datos)
         {
-            return true;
+            try
+            {
+                using (StreamReader archivoLectura = new StreamReader(archivo))
+                {
+                    datos = archivoLectura.ReadToEnd();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
         }
     }
 }
--- Solari.Rodolfo.2A.TP3/Archivos/Xml.cs
+++ Solari.Rodolfo.2A.TP3/Archivos/Xml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Excepciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Archivos
 {
@@ -10,12 +13,36 @@ namespace Archivos
     {
         public bool Guardar(string archivo, T datos)
         {
-            return true;
+            try
+            {
+                using (XmlTextWriter archivoEscritura = new XmlTextWriter(archivo, Encoding.UTF8))
+                {
+                    XmlSerializer serializador = new XmlSerializer(typeof(T));
+                    serializador.Serialize(archivoEscritura,datos);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
         }
 
         public bool Leer(string archivo, out T datos)
         {
-            return true;
+            try
+            {
+                using (XmlTextReader archivoLectura = new XmlTextReader(archivo))
+                {
+                    XmlSerializer serializador = new XmlSerializer(typeof(T));
+                    datos = (T)serializador.Deserialize(archivoLectura);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
         }
     }
 }
