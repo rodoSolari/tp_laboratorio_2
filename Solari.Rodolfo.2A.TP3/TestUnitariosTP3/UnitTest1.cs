62498570455b1f0ddcfa1c28370f153fd11454e5
rodoSolari <rodosolari033@gmail.com>
2020-11-01 21:52:46 -0300

    Agregado documentacion testUnitarios y volviendo a probarlos

--- Solari.Rodolfo.2A.TP3/TestUnitariosTP3/UnitTest1.cs
+++ Solari.Rodolfo.2A.TP3/TestUnitariosTP3/UnitTest1.cs
@@ -9,14 +9,20 @@ namespace TestUnitariosTP3
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Verifica que el nombre del alumno no se cargue debido a que contiene caracteres no validos
+        /// </summary>
         [TestMethod]
         public void TestNombreAlumnoInvalido()
         {
             Alumno alumno = new Alumno(1, "&$#34#@@", "Suarez", "13234333", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
-            bool nombre = alumno.Nombre == null;
-            Assert.IsTrue(nombre);
+            bool nombreValido = alumno.Nombre == null;
+            Assert.IsTrue(nombreValido);
         }
 
+        /// <summary>
+        /// Se espera una excepcion de tipo DniInvalido por contener caracteres no validos para un dni
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(DniInvalidoException))]
         public void TestDniInvalido()
@@ -24,6 +30,9 @@ namespace TestUnitariosTP3
             Profesor profesor = new Profesor(1, "Fernando", "Suarez", "EEEE3%$3", Persona.ENacionalidad.Argentino);
         }
 
+        /// <summary>
+        /// Verifica que la jornada no sea nula al agregar un alumno
+        /// </summary>
         [TestMethod]
         public void TestColeccionJornadaAgregarAlumno()
         {
